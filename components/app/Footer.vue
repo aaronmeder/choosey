1ece3f2faace2ad91fec1783e4821e4cbfd6df57
Aaron Meder <public.aaron@gmail.com>
2019-06-22 17:48:10 +0200

    Adding sass and basic styling

---
+++ components/app/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="app-footer">
+    My Footer
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+@import '../../assets/css/basics/variables';
+.app-footer {
+  color: $smokey-smoke-text;
+}
+</style>
---
+++ components/app/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>
+      <nuxt-link to="/">Choosey</nuxt-link>
+    </h1>
+    <div class="slogan">It is decided</div>
+  </div>
+</template>
