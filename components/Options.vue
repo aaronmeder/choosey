25090f5e3dd3f7aa2cf7e3f5504264b3c16f4bd9
Aaron Meder <public.aaron@gmail.com>
2019-06-25 21:10:10 +0200

    Introdue Add Item option

--- components/Options.vue
+++ components/Options.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="options">
     <p class="instructions">From what options do you want to decide?</p>
-    <form>
+    <form action="" method="POST">
       <p v-for="option in options" :key="option.index">
         <label for="option1">{{ option.index }}</label>
         <input
@@ -9,7 +9,32 @@
           name="option1"
           type="text"
           class="option"
+          value=""
         />
+      </p>
+      <p class="add-item">
+        <label for="option1"></label>
+        <button 
+          v-if="!isAdding" 
+          class="btn btn--add-item" 
+          @click="isAdding = !isAdding" 
+        >
+          Add Item
+        </button>
+        <div
+          v-if="isAdding" 
+          class="adding-item__fields"
+        >
+          <input
+            v-model="newOption"
+            name="option1"
+            type="text"
+            class="option"
+            value=""
+          />
+          <button class="btn btn--save-item" @click="addItem">Save Item</button> <button class="btn btn--cancel-add-item" @click="cancelAddItem">x</button>
+        </div>
+      <!-- eslint-disable-next-line -->
       </p>
       <p class="actions">
         <button @click="submitForm">Decide for me</button>
@@ -22,16 +47,38 @@
 export default {
   data: function() {
     return {
+      isAdding: false,
       options: [
         { value: 'Local Data Option One' },
         { value: 'Local Data Option Two' }
-      ]
+      ],
+      newOption: ''
     }
   },
   methods: {
     saveResults(allOptions, winningOption) {
       this.$store.commit('options/saveAllOptions', allOptions)
       this.$store.commit('options/saveWinningOption', winningOption)
+    },
+    addItem(event) {
+      event.preventDefault()
+      // if item isn't empty...
+      if(this.newOption !== '') {
+
+        // save the new option
+        this.options.push({ value: this.newOption });
+
+        // reset new option field
+        this.newOption = '';
+
+      }
+    },
+    cancelAddItem(event) {
+      event.preventDefault()
+      // reset add item status
+      this.isAdding = false;
+      // reset new option field
+      this.newOption = '';
     },
     submitForm(event) {
       event.preventDefault()
